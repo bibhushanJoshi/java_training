a8570bfbb04f04788258f3e69b04f3080fd32d4f
bibhushan <bibhushanjoshi@lftechnology.com>
2014-12-31 07:53:59 +0545

    creation of separate class

---
+++ src/dec29/checkPalindrome.java
@@ -0,0 +1,77 @@
+package dec29;
+
+import java.util.Scanner;
+
+/*
+*
+*
+* 2. A palindrome is a sequence of characters that reads the same backward as forward.
+* 	 For example, each of the following five-digit integers  * is a palindrome: 12321,
+* 	 55555, 45554 and 11611. Write an application that reads in a five-digit integer 
+*	and determines whether it is a palindrome. If the number is not five digits long, 
+* 	display an error message and allow the user to enter a new value.
+*
+*
+*/
+/**
+* 
+* This class scan a 5 digit integer number from the user. The scan number is then reversed
+* and later checked with original number to find whether the given number is a palindrome.
+* 
+* @author Bibhushan Raj Joshi
+*/
+
+public class checkPalindrome {
+	
+	private static Scanner scan;
+	
+	public static void main(String[] args) {
+		int temp;
+		int num = 0;
+		int revnum = 0;
+		String numS;
+		int tempNum = 0;
+		boolean wrongInput = false;
+		scan = new Scanner(System.in);
+
+		// check length of the number and whether the number is integer
+		do {
+			
+			do{
+				System.out.println("Please enter a 5 digit number:");
+				try{	
+					num = scan.nextInt();
+					wrongInput = false;
+				}
+				catch(Exception e){
+					System.out.println("Error : Please enter interger number \n");
+					scan.next();
+					wrongInput = true;
+				}
+			}while(wrongInput);
+			
+			tempNum = num;
+			numS = Integer.toString(num);
+			
+		} while (numS.length() != 5);
+		
+		
+		// reverse number generation
+		while (tempNum > 0) {
+			temp = tempNum % 10;
+			tempNum = tempNum / 10;
+			revnum = revnum * 10 + temp;
+		}
+		//System.out.println("The reverse number of "+num+" is: "+revnum);
+
+		// check if reverse number equals to original number
+		if (num == revnum)
+			System.out.println("The given number "+num+" is a palindrome number");
+		else
+			System.out.println("The given number "+num+" is not a palindrome number");
+
+	}
+
+	}
+
+
---
+++ src/dec29/numberSum.java
@@ -0,0 +1,10 @@
+package dec29;
+
+public class numberSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
