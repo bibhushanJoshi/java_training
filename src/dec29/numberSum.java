13ff16868bddf7b894265f6e213044f001691911
bibhushan <bibhushanjoshi@lftechnology.com>
2014-12-31 09:34:53 +0545

    author name comments added on top

--- src/dec29/numberSum.java
+++ src/dec29/numberSum.java
@@ -1,9 +1,43 @@
 package dec29;
 
+/*
+*
+*
+*
+* 2. Write a program to find the number of and sum of all integers greater than 100 
+* 	and less than 200 that are divisible by 7.
+*
+*
+*
+*/
+
+/**
+* 
+* This class uses switch case to give user the option to do: 
+* greater than 100 and less than 200 and divisible by 7 
+* 
+* 
+* @author Bibhushan Raj Joshi
+*/
+
 public class numberSum {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int min = 101;
+		int max = 200;
+		int sum = 0;
+		int count = 0;
+
+		// sum of number greater than 100 and less than 200
+		for (int i = min; i < max; i++) {
+			if (i % 7 == 0) {
+				count++;
+				System.out.println(count+"\t"+i);
+				sum = sum + i;
+			}
+		}
+		System.out.println("The required sum is:" + sum);
 
 	}
 
